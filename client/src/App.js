a839d9eb6ee36ea1713046db02424deff20598b2
Adarsh-NP <f20180806@goa.bits-pilani.ac.in>
2020-09-18 23:55:51 +0530

    installed react

---
+++ client/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import BookList from './components/BookList';
+import {ApolloClient, gql, ApolloProvider, InMemoryCache} from '@apollo/client'
+
+const client = new ApolloClient({
+  url: 'http://localhost:2000/graphql',
+  cache: new InMemoryCache
+})
+
+client
+  .query({
+    query: gql`
+      query books{
+        name
+        genre
+      }
+    `
+  })
+  .then(res => console.log(res))
+
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <BookList />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
